c217f4835b85b8cb357ea89471565f16fe2314e8
Howard Tyson <howard@howardtyson.com>
2012-01-31 09:35:18 -0500

    Moving mapData() method into the dataObject.

--- lib/Houston/Connector/ObjectConnector.php
+++ lib/Houston/Connector/ObjectConnector.php
@@ -2,5 +2,26 @@
 namespace Houston\Connector;
 
 class ObjectConnector implements ConnectorInterface {
-	
+  /**
+   * Map data from the controller into the data object.
+   *
+   * @param $dataObject
+   * @param $data
+   * @param $controllerMapping
+   */
+  public function mapData($input, $fieldMap) {
+    $data = new \stdClass;
+    $controllerMapping = $fieldMap;
+    foreach ($controllerMapping as $localName => $fieldData) {
+      if (isset($fieldData['load']) && !$fieldData['load']) {
+        continue;
+      }
+
+      $controllerName = $fieldData['field'];
+      if (isset($input->$controllerName)) {
+        $data->$localName = $input->$controllerName;
+      }
+    }
+    return $data;
+  }
 }
